f5b91898ec33fe4dd384d8cc9b2f3a42564579de
Andre-Tobois <tobois.andre@gmail.com>
2019-01-18 23:57:17 -0500

    feat: Compnenent 2 now has the react-spring feature to appear slowly on screen.

---
+++ src/components/Component2.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Spring } from "react-spring";
+
+export class Component2 extends Component {
+  render() {
+    return (
+      <Spring 
+      from={{ opacity: 0}}
+      to={{ opacity: 1}}
+      config={{ delay: 1000, duration: 2000}}>
+      
+        {props => (
+          <div style={props}>
+            <div style={a2style}>
+              <h1>Component 1</h1>
+              <p>"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"</p>
+            </div>
+          </div>
+        )}
+      </Spring>
+    )
+  }
+}
+
+const a2style = {
+  background: 'slateblue',
+  color:  'white',
+  padding: '1.5rem'
+}
+
+export default Component2
