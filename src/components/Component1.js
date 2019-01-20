71a54b177b4d1e26a09fd679f682bd3bcbff8fb4
Andre-Tobois <tobois.andre@gmail.com>
2019-01-19 23:33:47 -0500

    feat: Added a counter feature to Component 1.

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -12,6 +12,17 @@ export default function Component1() {
           <div style={a1style}>
             <h1>Component 1</h1>
             <p>"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"</p>
+            <Spring
+              from={{ number: 0 }}
+              to={{ number: 10 }}>
+              {props => (
+                <div style = {props}>
+                  <h1 style = {counter}>
+                    {props.number}
+                  </h1>
+                </div>
+              )}
+            </Spring>
           </div>
         </div>
       )}
@@ -24,3 +35,11 @@ const a1style = {
   color:  'black',
   padding: '1.5rem'
 }
+
+const counter = {
+  background: '#333',
+  textAlign: 'center',
+  width: '100px',
+  borderRadius: '50%',
+  margin: '1rem auto'
+};
